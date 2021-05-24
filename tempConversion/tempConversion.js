f61874e7ac850f1786b19abf8dbb73d1ba62674e
marloBarksdale <lyndenflood@gmail.com>
2021-05-24 12:51:01 -0400

    a few exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(num) {
 
+ num = (num - 32) *(5/9);
+
+return Math.round(num*10)/10;
 };
 
-const ctof = function() {
+const ctof = function(num) {
 
+   num = (num *(9/5)) + 32;
+
+  return Math.round(num*10)/10;
 };
 
 module.exports = {
